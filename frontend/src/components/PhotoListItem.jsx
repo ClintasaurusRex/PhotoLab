094e05af11f94ebd816bc685abea4b417d23d5cf
Clint Hiles <clinthiles1992@gmail.com>
2024-08-07 14:45:02 -0700

    first commit

---
+++ frontend/src/components/PhotoListItem.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "../styles/PhotoListItem.scss";
+
+
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
+const PhotoListItem = () => {
+ return (
+  <h1>Hello photo item</h1>
+ )
+};
+
+export default PhotoListItem;
