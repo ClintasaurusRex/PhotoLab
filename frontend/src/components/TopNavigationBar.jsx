dcedaf47c38a492a7403da90c7363f22f41beafb
Clint Hiles <clinthiles1992@gmail.com>
2024-08-08 15:26:02 -0700

    added the FavBadge as per requirments...ooops

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PhotoFavButton from "./PhotoFavButton";
+import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 import TopicList from './TopicList';
 
@@ -8,7 +8,7 @@ const TopNavigation = () => {
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList />
-      <PhotoFavButton />
+      <FavBadge />
     </div>
   )
 }
